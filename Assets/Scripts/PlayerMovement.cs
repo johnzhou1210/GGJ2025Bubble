b0923832c4f562a2dc223ead24014a33946bbd1f
Flame260 <162753205+Flame260@users.noreply.github.com>
2025-01-25 01:51:15 -0500

    playermovement
    Still need to work on making it more smooth and some issues

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed=1f;
+    public float swimForce=5f;
+    public float maxVerticalSpeed = 5f;
+    public float speedMultipplier = 2f;
+
+    public Rigidbody2D body;
+    private float horizontalInput;
+
+    public float swimCooldown = 0.5f;
+    public float lastSwimTime = -Mathf.Infinity; 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +22,38 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // prevent spamming that lauch the player into mar 
+        if(Input.GetKeyDown(KeyCode.W) && Time.time - lastSwimTime >= swimCooldown)
+        {
+            swimup();
+            lastSwimTime = Time.time;
+        }
+
+        // horizontal movement 
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        // swim faster and check if it being hold or not 
+        float newSpeed = speed;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            newSpeed *= speedMultipplier;
+            Debug.Log("RuN");
+        }
+        float targetSpeed = horizontalInput * newSpeed;
+        float smoothedSpeed = Mathf.Lerp(body.linearVelocity.x, targetSpeed, 0.1f);
+        body.linearVelocity = new Vector2(smoothedSpeed, body.linearVelocity.y);
+    }
+    private void swimup()
+    {
+        //reseting
+        float newVerticalSpeed = body.linearVelocity.y + swimForce;
+        //cap the vertical movement
+        newVerticalSpeed = Mathf.Clamp(newVerticalSpeed, -Mathf.Infinity, maxVerticalSpeed);
+        // apply force
+        body.linearVelocity = new Vector2(body.linearVelocity.x, newVerticalSpeed);
+
     }
 }
