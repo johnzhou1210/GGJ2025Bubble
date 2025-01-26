aa5fb029f0abd0aa3bb96f2c472d33e255775593
Flame260 <162753205+Flame260@users.noreply.github.com>
2025-01-26 13:46:10 -0500

    scene~~~

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -3,7 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+    public string scene;
+    public GameObject player;
     public void StartGame() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject == player)
+        {
+            SceneManager.LoadScene("scene");
+        }
     }
 }
